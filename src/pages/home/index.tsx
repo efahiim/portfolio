0e31b03e7ef10fd1fbe6c9e745ae3613e3566911
efahiim <efahiim@gmail.com>
2024-04-22 00:26:15 +0400

    Adjusted position of tip

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -154,7 +154,7 @@ const Home: React.FunctionComponent = () => {
               | React.LegacyRef<HTMLParagraphElement>
               | undefined
           }
-          className="sm:text-xl sm:leading-snug text-center px-8 text-black mx-auto w-full opacity-0 absolute bottom-5"
+          className="sm:text-xl sm:leading-snug text-center px-8 text-black mx-auto w-full opacity-0 absolute bottom-12"
         >
           Tip:{" "}
           {(isMobile || isTablet) && (
