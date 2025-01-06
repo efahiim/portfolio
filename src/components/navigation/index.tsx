ac6f4ab12fa4c244ea16a94bd46ff5dfc5083819
IMF <efahiim@gmail.com>
2025-01-06 13:26:29 +0000

    Removed splash screen

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+
+import wind from "@assets/sounds/wind.mp3";
+import plane from "@assets/sounds/plane.mp3";
+import sound from "@assets/icons/sound.png";
+import noSound from "@assets/icons/no-sound.png";
 
 const Navigation: React.FunctionComponent = () => {
   const { pathname } = useLocation();
+  const windAudioRef = useRef(new Audio(wind));
+  const planeAudioRef = useRef(new Audio(plane));
+  const [isPlayingMusic, setIsPlayingMusic] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isPlayingMusic) {
+      windAudioRef.current.play();
+      planeAudioRef.current.play();
+    }
+
+    return () => {
+      windAudioRef.current.pause();
+      planeAudioRef.current.pause();
+    };
+  }, [isPlayingMusic]);
+
+  windAudioRef.current.volume = 0.05;
+  windAudioRef.current.loop = true;
+  planeAudioRef.current.volume = 0.01;
+  planeAudioRef.current.loop = true;
+  
   const isHomePage = pathname === "/";
 
   return (
@@ -26,6 +52,17 @@ const Navigation: React.FunctionComponent = () => {
       >
         <p className="blue-gradient_text">{!isHomePage ? <>IMF</> : <>I</>}</p>
       </NavLink>
+      {isHomePage && (
+        <button
+          className="border-0 outline-none px-5 py-2 cursor-none"
+          style={{
+            clipPath: "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)",
+          }}
+          onClick={() => setIsPlayingMusic((prevState) => !prevState)}
+        >
+          <img src={isPlayingMusic ? sound : noSound} alt="Test" width={32} />
+        </button>
+      )}
       {!isHomePage && (
         <nav className="flex text-lg gap-7 font-medium">
           <NavLink
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,17 +12,10 @@ import Plane from "@models/plane";
 
 import { PlaneData } from "@interfaces/index";
 
-import wind from "@assets/sounds/wind.mp3";
-import plane from "@assets/sounds/plane.mp3";
-
 const Home: React.FunctionComponent = () => {
-  const windAudioRef = useRef(new Audio(wind));
-  const planeAudioRef = useRef(new Audio(plane));
   const tipRef = useRef();
-  const splashRef = useRef<HTMLDivElement>(null);
   const [isMoving, setIsMoving] = useState<boolean>(false);
   const [currentStage, setCurrentStage] = useState<number>(1);
-  const [isPlayingMusic, setIsPlayingMusic] = useState<boolean>(false);
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const [isTablet, setIsTablet] = useState<boolean>(false);
   const [isLaptop, setIsLaptop] = useState<boolean>(false);
@@ -41,26 +34,7 @@ const Home: React.FunctionComponent = () => {
     if (screenSize >= 1440 && screenSize < 1920) {
       setIsLaptop(true);
     }
-
-    console.log(isMobile, isTablet, isLaptop);
   }, [window.innerWidth]);
-
-  useEffect(() => {
-    if (isPlayingMusic) {
-      windAudioRef.current.play();
-      planeAudioRef.current.play();
-    }
-
-    return () => {
-      windAudioRef.current.pause();
-      planeAudioRef.current.pause();
-    };
-  }, [isPlayingMusic]);
-
-  windAudioRef.current.volume = 0.05;
-  windAudioRef.current.loop = true;
-  planeAudioRef.current.volume = 0.01;
-  planeAudioRef.current.loop = true;
 
   useGSAP(() => {
     gsap.to(tipRef.current as unknown as gsap.TweenTarget, {
@@ -108,126 +82,72 @@ const Home: React.FunctionComponent = () => {
   const planeData = adjustPlane();
 
   return (
-    <>
-      <section className="w-full h-screen relative">
-        <div className="absolute top-[15%] left-0 right-0 z-10 flex items-center justify-center">
-          {currentStage && <Popup currentStage={currentStage} />}
-        </div>
-
-        <Canvas
-          className="w-full h-screen bg-transparent"
-          camera={{ near: 0.1, far: 1000 }}
-        >
-          <Suspense fallback={<Loader />}>
-            <directionalLight position={[1, 1, 1]} intensity={2} />
-
-            <ambientLight intensity={0.5} />
-
-            <hemisphereLight
-              // @ts-expect-error: Three.js
-              skyColor="#b1e1ff"
-              groundColor="#000000"
-              intensity={1}
-            />
-
-            <Sky />
-
-            <Plane
-              // @ts-expect-error: Three.js
-              scale={planeData.scale}
-              position={planeData.position}
-              rotation={planeData.rotation}
-              isMoving={isMoving}
-              setIsMoving={setIsMoving}
-              currentStage={currentStage}
-              setCurrentStage={setCurrentStage}
-              isMobile={isMobile}
-              isTablet={isTablet}
-              isLaptop={isLaptop}
-            />
-          </Suspense>
-        </Canvas>
-
-        <p
-          ref={
-            tipRef as unknown as
-              | React.LegacyRef<HTMLParagraphElement>
-              | undefined
-          }
-          className="sm:text-xl sm:leading-snug text-center px-8 text-black mx-auto w-full opacity-0 absolute bottom-[6rem] lg:bottom-10"
-        >
-          Tip:{" "}
-          {(isMobile || isTablet) && (
-            <>
-              Tap the <span className="font-semibold">right</span> or the{" "}
-              <span className="font-semibold">left</span> side of the screen to
-              fly.
-            </>
-          )}
-          {window.innerWidth >= 1440 && (
-            <>
-              You can either click on the{" "}
-              <span className="font-semibold">right</span> and the{" "}
-              <span className="font-semibold">left</span> of the screen or use
-              the keyboard <span className="font-semibold">arrow right</span>{" "}
-              and <span className="font-semibold">arrow left</span> keys to fly.
-            </>
-          )}
-        </p>
-      </section>
-
-      <div
-        ref={splashRef}
-        className="absolute w-full h-screen top-0 left-0 bg-black/85 z-10 flex justify-center items-center"
-      >
-        <div
-          className="bg-white flex flex-col gap-5 justify-center items-center py-5 mx-4 rounded-sm"
-        >
-          <p className="sm:text-xl sm:leading-snug font-medium text-center text-black px-5">Do you want to play music for the best experience?</p>
-          <div className="flex gap-5">
-            <button
-              className="bg-[#00c6ff] border-0 outline-none px-5 py-2 cursor-none"
-              style={{
-                clipPath:
-                  "polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0% 50%)",
-              }}
-              onClick={() => {
-                setIsPlayingMusic((prevState) => !prevState);
-                if (splashRef.current) {
-                  gsap.to(splashRef.current, {
-                    opacity: 0,
-                    duration: 1,
-                  });
-                  gsap.to(splashRef.current, {
-                    display: "none",
-                    delay: 1,
-                  });
-                }
-              }}
-            >
-              Yes
-            </button>
-            <button
-              className="bg-[#00c6ff] border-0 outline-none px-5 py-2 cursor-none"
-              style={{
-                clipPath:
-                  "polygon(15% 0, 85% 0, 100% 50%, 85% 100%, 15% 100%, 0% 50%)",
-              }}
-              onClick={() => {
-                if (splashRef.current) {
-                  gsap.to(splashRef.current, {
-                    x: "-100%",
-                    duration: 1
-                  });
-                }
-              }}
-            >
-              No
-            </button>
-          </div>
-        </div>
+    <section className="w-full h-screen relative">
+      <div className="absolute top-[15%] left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <Popup currentStage={currentStage} />}
       </div>
-    </>
+
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+
+          <ambientLight intensity={0.5} />
+
+          <hemisphereLight
+            // @ts-expect-error: Three.js
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+
+          <Sky />
+
+          <Plane
+            // @ts-expect-error: Three.js
+            scale={planeData.scale}
+            position={planeData.position}
+            rotation={planeData.rotation}
+            isMoving={isMoving}
+            setIsMoving={setIsMoving}
+            currentStage={currentStage}
+            setCurrentStage={setCurrentStage}
+            isMobile={isMobile}
+            isTablet={isTablet}
+            isLaptop={isLaptop}
+          />
+        </Suspense>
+      </Canvas>
+
+      <p
+        ref={
+          tipRef as unknown as
+            | React.LegacyRef<HTMLParagraphElement>
+            | undefined
+        }
+        className="sm:text-xl sm:leading-snug text-center px-8 text-black mx-auto w-full opacity-0 absolute bottom-[6rem] lg:bottom-10"
+      >
+        Tip:{" "}
+        {(isMobile || isTablet) && (
+          <>
+            Tap the <span className="font-semibold">right</span> or the{" "}
+            <span className="font-semibold">left</span> side of the screen to
+            fly.
+          </>
+        )}
+        {window.innerWidth >= 1440 && (
+          <>
+            You can either click on the{" "}
+            <span className="font-semibold">right</span> and the{" "}
+            <span className="font-semibold">left</span> of the screen or use
+            the keyboard <span className="font-semibold">arrow right</span>{" "}
+            and <span className="font-semibold">arrow left</span> keys to fly.
+          </>
+        )}
+      </p>
+    </section>
   );
 };
 
